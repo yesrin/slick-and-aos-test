d9f6490255aa3162af6918299ae68eee225d8244
yesrin <asjs11@naver.com>
2022-01-14 23:00:35 +0900

    AOS 적용중

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { BrowserRouter as Router } from "react-router-dom";
+import Imagescroll from "./components/Image scroll";
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <body>
+        <Imagescroll />
+      </body>
+      <footer></footer>
     </Router>
   );
 }
