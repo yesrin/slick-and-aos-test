fe8420d1c5d6d59953720d0f6a2af7b68eb696cf
yesrin <asjs11@naver.com>
2022-01-16 18:26:42 +0900

    imge scroll scss작업중

---
+++ src/components/ImageScroll.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+import "./ImageScroll.scss";
+
+const Imagescroll = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+    });
+  });
+
+  return (
+    <div className="container">
+      <div className="box_right" data-aos="fade-right">
+        <p>AOS 테스트1</p>
+      </div>
+      <div className="box_left" data-aos="fade-left">
+        <p>AOS 테스트2</p>
+      </div>
+      <div className="box_right" data-aos="fade-right">
+        <p>AOS 테스트3</p>
+      </div>
+      <div className="box_left" data-aos="fade-left">
+        <p>AOS 테스트4</p>
+      </div>
+    </div>
+  );
+};
+
+export default Imagescroll;
